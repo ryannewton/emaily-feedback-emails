ca44fdc49eede75ae804ed0b7b08b4a35684cbdf
Ryan Newton <newton1988@gmail.com>
2018-12-27 21:14:42 -0500

    Changed order of operations to fix routing bug

--- index.js
+++ index.js
@@ -22,6 +22,9 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
+
 if (process.env.NODE_ENV === 'production') {
   // If any request comes in and we don't have a
   // route handler, check the 'client/build' folder
@@ -34,9 +37,6 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
-
-require('./routes/authRoutes')(app);
-require('./routes/billingRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
